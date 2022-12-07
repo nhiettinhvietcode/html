14781f82bd6d87dffa1ec99194d76cda93ed964f
Nhiệt tình viết code <108312840+nhiettinhvietcode@users.noreply.github.com>
2022-12-07 23:23:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
